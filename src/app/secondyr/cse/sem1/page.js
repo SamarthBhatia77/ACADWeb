3704c2e8ea906979a6ce57181ed5f8101ace9ce0
SamarthBhatia77 <samarthbhatia77@gmail.com>
2025-08-09 11:50:28 +0530

    2nd year links updated

--- src/app/secondyr/cse/sem1/page.js
+++ src/app/secondyr/cse/sem1/page.js
@@ -5,20 +5,20 @@ export default function firstyr() {
       <p className="text-[20px] my-3 font-bold">Select Course to view rescources</p>
       <ul className="w-full">
         <li className="border border-[#8c8c8c] rounded-[10px]  my-4 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">OOPS</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1qz_q2KdoW0bh97mCcovo8m5-bA-pu5xF"  target="_blank">OOPS</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Logic in CS</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1T_RtXHnMyfJGRl_wuyLRMLnbvLIZofQ2"  target="_blank">Logic in CS</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Digital Design</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1DFiRa4OSL3VbEOjkHHQrDpHnXO4x7Xox"  target="_blank">Digital Design</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Discrete structures in CS</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1_9hwkIFCtg-OLTep5mYcTkTg5O-tPNW5"  target="_blank">Discrete structures in CS</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">POE</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1mcL9AYtqTH_nmC_fa_eXzzh0SIrXsqoe"  target="_blank">POE</a></li>
         
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Mathematics-3</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1exn_hFuIDecU6Uy-J6-_7v_RwK6Ovcoi"  target="_blank">Mathematics-3</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  font-extrabold mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">TEXTBOOKS Sem-1</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1jXhXswfTxvxkx05Ax-_u2JnoYYbZY7An"  target="_blank">TEXTBOOKS Sem-1</a></li>
       </ul>
     </div>
     );
--- src/app/secondyr/cse/sem2/page.js
+++ src/app/secondyr/cse/sem2/page.js
@@ -1,19 +1,19 @@
 import React from "react";
 export default function firstyr() {
     return(
-        <div id="container"className="flex flex-col items-center justify-center  text-center font-mono text-black bg-white">
+      <div id="container" className="flex flex-col items-center justify-center  text-center font-mono text-black bg-white">
       <p className="text-[20px] my-3 font-bold">Select Course to view rescources</p>
       <ul className="w-full">
         <li className="border border-[#8c8c8c] rounded-[10px]  my-4 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">DSA</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1kLJ7me8gU8Xf5in9ZZhbG1C8fDpKv5S-" target="_blank">DSA</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Database Systems</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1ptM728sTyHYb1iaYVnbT-LsZ3r8vfVL8" target="_blank">Database Systems</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Microprocessors & Interfacing</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1unHCApJq_HLqAg3pKMNEK1Z3qbLVoAPq" target="_blank">Microprocessors & Interfacing</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Public Administration</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1vHINVzGUR-xWeCKZM49lML_I31yZYC4Q" target="_blank">Public Administration</a></li>
         <li className="border border-[#8c8c8c] rounded-[10px]  mb-3 lg:px-40 py-3 cursor-pointer hover:bg-[#d1d1d1] transition 0.4s ease">
-          <a href="#">Intro to Globalization</a></li>
+          <a href="https://drive.google.com/drive/u/1/folders/1OvWixEDkIw1sWt9rGzFn42pftnS-N1Il" target="_blank">Intro to Globalization</a></li>
       </ul>
     </div>
     );
